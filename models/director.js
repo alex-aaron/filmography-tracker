9351bd04979eeeac17dccbf4b64e32d9e323df66
Alex Aaron <alexfrancisaaron@gmail.com>
2025-09-16 20:01:52 -0500

    added models directory, views directory, index.ejs, director schema

---
+++ models/director.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const directorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  films: {
+    type: [Object],
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Director", directorSchema);
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const Director = require('./models/director');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index');
 })
+
 app.listen(port, () => {
   console.log('Connection successful');
 })
